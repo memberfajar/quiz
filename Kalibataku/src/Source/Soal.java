8bc34910106b993e6849edd84787a168babe7694
memberfajar <memberfajar@gmail.com>
2015-04-20 11:59:17 +0700

    quiz 1

---
+++ Kalibataku/src/Source/Soal.java
@@ -0,0 +1,25 @@
+package Source;
+
+public class Soal {
+	public int pangkat(int bil, int pangkat){
+		int hasil = 0;
+		int n = 1;
+		while(n<= pangkat){
+			n++;
+			hasil *= bil;
+		}
+		return hasil;
+	}
+	
+	public int factorial(int bil){
+		int hasil = 1;
+		for (int i = 1; i <= bil; i++) {
+			hasil *= i;
+		}
+		return hasil;		
+	}
+	
+	public int perkalian(int bilA, int bilB){
+		return bilA * bilB;
+	}
+}
